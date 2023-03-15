15fc7d05f2e58cc92acd85983c8a8c21030bcb1a
Krisna Mughni <68163712+krisnamughni24@users.noreply.github.com>
2023-03-15 19:05:10 +0700

    Add files via upload

---
+++ webpack---_wix_wix_perf_measure/entries/fonts.js
@@ -0,0 +1,11 @@
+// @ts-check
+import { isFont } from "../utils/observeResources.js";
+import collectResources from "../utils/collectResources.js";
+
+/**
+ * Get font resources attributes
+ * @param {import('../utils/utils.js').State} state
+ */
+export default function fonts(state) {
+  return collectResources(state, "fonts", { filter: isFont });
+}
